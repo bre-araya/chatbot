5f60d2033a92e4f5a063511a4cd07638d229afdd
Berihu <berihuaraya374@gmail.com>
2026-07-10 15:24:27 +0300

    authentication

--- chatbot/urls_auth.py
+++ chatbot/urls_auth.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('accounts/create/', views.create_account, name='create_account'),
+    # Backwards-compatible alias for older frontend code
+    path('accounts/register/', views.create_account, name='register'),
     path('accounts/login/', views.login, name='login'),
     path('accounts/', views.get_all_accounts, name='get_all_accounts'),
     path('accounts/forgot_password/', views.forgot_password, name='forgot_password'),
--- helpdesk_frontend/src/components/SignupForm.jsx
+++ helpdesk_frontend/src/components/SignupForm.jsx
@@ -14,7 +14,7 @@ function SignupForm({ onSuccess = () => {} }) {
     setError("");
     setSuccess("");
 
-    const res = await fetch("http://127.0.0.1:8000/api/accounts/register/", {
+    const res = await fetch("http://127.0.0.1:8000/api/accounts/create/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
